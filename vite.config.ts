20387b329b6492b34adc97f0714c57ee000471ce
arashms1994 <arash.ms1994@gmail.com>
2025-06-28 15:44:16 +0330

    Feat: install shadCN for project

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
-  base: "./", // آدرس ریشه در SharePoint
+  base: "./",
   build: {
     outDir: "dist",
     assetsDir: ".",
@@ -14,6 +15,10 @@ export default defineConfig({
       },
     },
   },
-
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   plugins: [react()],
 });
