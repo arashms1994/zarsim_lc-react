101a62ca3d612211b1ac71f7b79622dbd48958fa
arashms1994 <arash.ms1994@gmail.com>
2025-07-15 16:55:46 +0330

    Refactor  codes

--- src/components/openning/Openning.tsx
+++ src/components/openning/Openning.tsx
@@ -54,6 +54,17 @@ const Openning = () => {
     }
   };
 
+  if (!faktorNumber) {
+    return (
+      <div className="mt-12 flex flex-col items-center justify-center gap-2">
+        <div className="w-10 h-10 border-4 border-gray-200 border-t-blue-500 rounded-full animate-spin"></div>
+        <span className="mt-2 text-base font-medium text-gray-800">
+          در حال بارگذاری...
+        </span>
+      </div>
+    );
+  }
+
   return (
     <div>
       <form
