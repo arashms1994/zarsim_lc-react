ff91548130cee38d9c7ed9ea250cb33addfffb6c
arashms1994 <arash.ms1994@gmail.com>
2025-07-16 15:38:15 +0330

    fix: refactor some files

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,14 +1,16 @@
 export const LCOpenningDates = [
-  { value: "openningDate", label: "تاریخ گشایش" },
-  { value: "communicationDate", label: "تاریخ ابلاغ" },
-  { value: "shippingDate", label: "تاریخ حمل/بارنامه" },
-  { value: "invoiceDate", label: "تاریخ فاکتور" },
+  { value: "از تاریخ گشایش", label: "از تاریخ گشایش" },
+  { value: "از تاریخ ابلاغ", label: "از تاریخ ابلاغ" },
+  { value: "از تاریخ حمل/بارنامه", label: "از تاریخ حمل/بارنامه" },
+  { value: "از تاریخ فاکتور", label: "از تاریخ فاکتور" },
 ];
 
 export const settlementDates = [
+  { value: "30", label: "30" },
   { value: "45", label: "45" },
   { value: "60", label: "60" },
   { value: "75", label: "75" },
   { value: "90", label: "90" },
   { value: "120", label: "120" },
+  { value: "180", label: "180" },
 ];
