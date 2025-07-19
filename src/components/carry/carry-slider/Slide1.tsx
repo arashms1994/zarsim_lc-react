a49480db90c383bd98f4bff8975686e7fffbe319
arashms1994 <arash.ms1994@gmail.com>
2025-07-19 14:36:32 +0330

    Fix: refactor slides

--- src/components/carry/carry-slider/Slide1.tsx
+++ src/components/carry/carry-slider/Slide1.tsx
@@ -1,17 +1,7 @@
-import SlideLayout from "@/components/ui/SliderLayout";
 import type { ICarrySliderProps } from "@/utils/type";
 
-const Slide1: React.FC<ICarrySliderProps> = ({ setPage }) => {
-  return (
-    <SlideLayout
-      title="حمل"
-      onNext={() => setPage(2)}
-      onPrev={() => {}}
-      disablePrev={true}
-    >
-      <h2 className="text-xl">این محتوای اسلاید 1 است</h2>
-    </SlideLayout>
-  );
+const Slide1: React.FC<ICarrySliderProps> = () => {
+  return <h2 className="text-xl">این محتوای اسلاید 1 است</h2>;
 };
 
 export default Slide1;
--- src/components/carry/carry-slider/Slide3.tsx
+++ src/components/carry/carry-slider/Slide3.tsx
@@ -1,16 +1,7 @@
-import SlideLayout from "@/components/ui/SliderLayout";
 import type { ICarrySliderProps } from "@/utils/type";
 
-const Slide3: React.FC<ICarrySliderProps> = ({ setPage }) => {
-  return (
-    <SlideLayout
-      title="ارسال اسناد به بانک"
-      onNext={() => setPage(4)}
-      onPrev={() => setPage(2)}
-    >
-      <h2 className="text-xl">این محتوای اسلاید 3 است</h2>
-    </SlideLayout>
-  );
+const Slide3: React.FC<ICarrySliderProps> = () => {
+  return <h2 className="text-xl">این محتوای اسلاید 3 است</h2>;
 };
 
 export default Slide3;
