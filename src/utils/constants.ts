364cdda8100bf1610c3c0e8adacb81f350a7ef3b
arashms1994 <arash.ms1994@gmail.com>
2025-07-20 12:14:03 +0330

    Feat: Add uploaders label

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,11 +1,11 @@
-export const LCOpenningDates = [
+export const LC_OPENNING_DATES = [
   { value: "از تاریخ گشایش", label: "از تاریخ گشایش" },
   { value: "از تاریخ ابلاغ", label: "از تاریخ ابلاغ" },
   { value: "از تاریخ حمل/بارنامه", label: "از تاریخ حمل/بارنامه" },
   { value: "از تاریخ فاکتور", label: "از تاریخ فاکتور" },
 ];
 
-export const settlementDates = [
+export const LC_SETTLEMENT_DATES = [
   { value: "30", label: "30" },
   { value: "45", label: "45" },
   { value: "60", label: "60" },
@@ -13,4 +13,16 @@ export const settlementDates = [
   { value: "90", label: "90" },
   { value: "120", label: "120" },
   { value: "180", label: "180" },
+];
+
+export const FIRST_SLIDE_DOCS = [
+  { label: "فاکتور فروش", value: "faktor" },
+  { label: "بارنامه", value: "barnameh" },
+  { label: "برگه باسکول", value: "baskool" },
+];
+
+export const SECOND_SLIDE_DOCS = [
+  { label: "پکینگ", value: "paching" },
+  { label: "گواهی بازرسی", value: "govahibazrasi" },
+  { label: "صورت مجلس تحویل و تحول", value: "tahvilotahavol" },
 ];
