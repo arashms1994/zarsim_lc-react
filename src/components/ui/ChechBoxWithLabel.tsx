d4ad7f27573c8f14b3c0af27a312e6c8033306e0
arashms1994 <arash.ms1994@gmail.com>
2025-07-30 09:44:42 +0330

    Feat: Add Checkbox component

---
+++ src/components/ui/ChechBoxWithLabel.tsx
@@ -0,0 +1,13 @@
+import { Checkbox } from "./checkbox";
+import { Label } from "./label";
+
+const CheckboxWithLabel = ({ id, label, checked, onChange }) => {
+  return (
+    <div className="flex items-center gap-3">
+      <Checkbox id={id} checked={checked} onCheckedChange={onChange} />
+      <Label htmlFor={id}>{label}</Label>
+    </div>
+  );
+};
+
+export default CheckboxWithLabel;
