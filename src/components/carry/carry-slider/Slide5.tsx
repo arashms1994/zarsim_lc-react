aa114aac5186bb63b44accd71a106b57b67d0631
arashms1994 <arash.ms1994@gmail.com>
2025-08-02 14:50:55 +0330

    Feat: Add slides

--- src/components/carry/carry-slider/Slide5.tsx
+++ src/components/carry/carry-slider/Slide5.tsx
@@ -1,7 +1,41 @@
+import FileUploader from "@/components/file-uploader/FileUploader";
 import type { ICarrySlideProps } from "@/utils/type";
+import { useRef } from "react";
 
-const Slide5: React.FC<ICarrySlideProps> = () => {
-  return <h2 className="text-xl">این محتوای اسلاید 5 است</h2>;
+const Slide5: React.FC<ICarrySlideProps> = ({ faktorNumber, GUID }) => {
+  const sendRef = useRef<any>(null);
+
+  const label = "رسید تایید اسناد توسط بانک";
+  const subFolder = GUID;
+  const docType = "taeidasnad";
+
+  const uploadFile = async () => {
+    if (sendRef.current) {
+      await sendRef.current.uploadFile();
+    }
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center gap-5">
+      <div className="w-full min-w-[500px] flex justify-between items-center gap-5">
+        <label className="text-[22px] font-medium">{`آپلود ${label}:`}</label>
+        <FileUploader
+          ref={sendRef}
+          orderNumber={faktorNumber}
+          subFolder={subFolder}
+          docType={docType}
+        />
+      </div>
+
+      <button
+        type="button"
+        className="border-none rounded-lg min-w-[200px] p-2 text-[18px] font-semibold bg-blue-600 text-white transition-all duration-300 cursor-pointer hover:bg-blue-900"
+        onClick={uploadFile}
+      >
+        {`آپلود ${label}`}
+      </button>
+    </div>
+  );
 };
 
 export default Slide5;
--- src/components/carry/carry-slider/Slide6.tsx
+++ src/components/carry/carry-slider/Slide6.tsx
@@ -1,8 +1,41 @@
+import FileUploader from "@/components/file-uploader/FileUploader";
 import type { ICarrySlideProps } from "@/utils/type";
-// import { Navigate } from "react-router";
+import { useRef } from "react";
 
-const Slide6: React.FC<ICarrySlideProps> = () => {
-  return <h2 className="text-xl">این محتوای اسلاید 6 است</h2>;
+const Slide6: React.FC<ICarrySlideProps> = ({ faktorNumber, GUID }) => {
+  const sendRef = useRef<any>(null);
+
+  const label = "رسید واریز مبلغ";
+  const subFolder = GUID;
+  const docType = "residvariz";
+
+  const uploadFile = async () => {
+    if (sendRef.current) {
+      await sendRef.current.uploadFile();
+    }
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center gap-5">
+      <div className="w-full min-w-[500px] flex justify-between items-center gap-5">
+        <label className="text-[22px] font-medium">{`آپلود ${label}:`}</label>
+        <FileUploader
+          ref={sendRef}
+          orderNumber={faktorNumber}
+          subFolder={subFolder}
+          docType={docType}
+        />
+      </div>
+
+      <button
+        type="button"
+        className="border-none rounded-lg min-w-[200px] p-2 text-[18px] font-semibold bg-blue-600 text-white transition-all duration-300 cursor-pointer hover:bg-blue-900"
+        onClick={uploadFile}
+      >
+        {`آپلود ${label}`}
+      </button>
+    </div>
+  );
 };
 
 export default Slide6;
