95c5e4cb004d9d146840084d49756712bfd718df
arashms1994 <arash.ms1994@gmail.com>
2025-08-04 14:21:23 +0330

    Fix upload link ui and date picker state issue

--- src/components/persian-date-picker/PersianDatePicker.tsx
+++ src/components/persian-date-picker/PersianDatePicker.tsx
@@ -3,15 +3,24 @@ import persian from "react-date-object/calendars/persian";
 import type { IPersianDatePickerProps } from "@/utils/type";
 import persian_fa from "react-date-object/locales/persian_fa";
 import { CalendarSearch } from "lucide-react";
+import DateObject from "react-date-object";
 
 const PersianDatePicker: React.FC<IPersianDatePickerProps> = ({
   value,
   onChange,
 }) => {
+  const dateObject = value
+    ? new DateObject({
+        date: value,
+        calendar: persian,
+        locale: persian_fa,
+      })
+    : null;
+
   return (
     <div className="w-full max-w-[230px] min-h-[34px] border-2 border-[#ababab] rounded-lg flex items-center justify-between pl-2">
       <DatePicker
-        value={value}
+        value={dateObject}
         calendar={persian}
         locale={persian_fa}
         calendarPosition="bottom-right"
