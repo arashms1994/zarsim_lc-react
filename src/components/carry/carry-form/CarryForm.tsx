1987f63fcefeca8315d07b8bda85e6cf7e0f4225
arashms1994 <arash.ms1994@gmail.com>
2025-08-06 11:51:46 +0330

    Feat; add carry form modal

---
+++ src/components/carry/carry-form/CarryForm.tsx
@@ -0,0 +1,97 @@
+import { useForm } from "react-hook-form";
+import { CarryFormSchema } from "@/utils/validation";
+import PersianDatePicker from "@/components/persian-date-picker/PersianDatePicker";
+import { zodResolver } from "@hookform/resolvers/zod";
+import type { ICarryForm } from "@/utils/type";
+
+const CarryForm = ({ onSubmit, isSubmitting }: ICarryForm) => {
+  const {
+    watch,
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<CarryFormSchema>({
+    resolver: zodResolver(CarryFormSchema),
+    defaultValues: {
+      Count: "",
+      Total: "",
+      Title: "",
+      Date: "",
+    },
+  });
+
+  return (
+    <div className="w-full max-w-[500px] flex justify-between items-center gap-5 my-5">
+      <form
+        className="flex flex-col justify-center items-center gap-5 py-5"
+        onSubmit={(e) => {
+          handleSubmit((data) => {
+            onSubmit(data);
+          })(e);
+        }}
+      >
+        <div className="w-full max-w-[500px] flex justify-between items-center gap-5">
+          <label className="text-[22px] font-medium">شماره فاکتور:</label>
+
+          <input
+            className="w-[230px] min-h-[30px] px-1 py-[2px] text-[18px] font-normal text-gray-700 rounded-lg border-2 border-[#ababab]"
+            {...register("Title")}
+          />
+          {errors.Title && (
+            <p className="text-red-500 text-sm">{errors.Title.message}</p>
+          )}
+        </div>
+
+        <div className="w-full max-w-[500px] flex justify-between items-center gap-5">
+          <label className="text-[22px] font-medium">تاریخ فاکتور:</label>
+          <PersianDatePicker
+            value={watch("Date") || null}
+            onChange={(date) => setValue("Date", date)}
+          />
+
+          {errors.Date && (
+            <p className="text-red-500 text-sm">{errors.Date.message}</p>
+          )}
+        </div>
+
+        <div className="w-full max-w-[500px] flex justify-between items-center gap-5">
+          <label className="text-[22px] font-medium">مجموع متراژ: </label>
+
+          <input
+            className="w-[230px] min-h-[30px] px-1 py-[2px] text-[18px] font-normal text-gray-700 rounded-lg border-2 border-[#ababab]"
+            {...register("Count")}
+          />
+          {errors.Count && (
+            <p className="text-red-500 text-sm">{errors.Count.message}</p>
+          )}
+        </div>
+
+        <div className="w-full max-w-[500px] flex justify-between items-center gap-5">
+          <label className="text-[22px] font-medium">مبلغ کل(ریال):</label>
+
+          <input
+            className="w-[230px] min-h-[30px] px-1 py-[2px] text-[18px] font-normal text-gray-700 rounded-lg border-2 border-[#ababab]"
+            {...register("Total")}
+          />
+          {errors.Total && (
+            <p className="text-red-500 text-sm">{errors.Total.message}</p>
+          )}
+        </div>
+
+        <button
+          type="submit"
+          className="border-none rounded-lg min-w-[200px] mt-5 p-3 text-[18px] font-semibold bg-blue-600 text-white transition-all duration-300 cursor-pointer hover:bg-blue-900"
+          disabled={isSubmitting}
+          onClick={() =>
+            console.log("Submit button clicked, isSubmitting:", isSubmitting)
+          }
+        >
+          {isSubmitting ? "در حال ثبت..." : "ثبت اطلاعات"}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CarryForm;
--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,16 +1,23 @@
 import { z } from "zod";
 
-export const openingFormSchema = z.object({
+export const openningFormSchema = z.object({
   LCNumber: z.string().min(1, "شماره اعتبار الزامی است"),
-  LCTotal: z
-    .string()
-    .refine((val) => !isNaN(Number(val.replace(/,/g, ""))), {
-      message: "مبلغ باید عددی باشد",
-    }),
+  LCTotal: z.string().refine((val) => !isNaN(Number(val.replace(/,/g, ""))), {
+    message: "مبلغ باید عددی باشد",
+  }),
   tarikhgoshayesh: z.string().min(1, "تاریخ گشایش الزامی است"),
   tarikheblagh: z.string().min(1, "تاریخ ابلاغ الزامی است"),
   mabnavalue: z.string().min(1, "مبدا زمان تسویه الزامی است"),
   tarikhmabnavalue: z.string().min(1, "مدت زمان تسویه الزامی است"),
 });
 
-export type OpeningFormSchema = z.infer<typeof openingFormSchema>;
+export type OpenningFormSchema = z.infer<typeof openningFormSchema>;
+
+export const CarryFormSchema = z.object({
+  Count: z.string().min(1, "وارد کردن متراژ کل بار الزامیست."),
+  Total: z.string().min(1, "وارد کردن ارزش کل بار الزامیست."),
+  Title: z.string().min(1, "شماره فاکتور(مالی) الزامیست."),
+  Date: z.string().min(1, "تاریخ فاکتور الزامی است."),
+});
+
+export type CarryFormSchema = z.infer<typeof CarryFormSchema>;
