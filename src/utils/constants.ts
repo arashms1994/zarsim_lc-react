725e2a71d9f2fb2aa87477ff365395bb014bc709
arashms1994 <arash.ms1994@gmail.com>
2025-08-23 12:41:57 +0330

    Feat add constants for table refactoring phase

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,14 @@
+import { Bounce } from "react-toastify";
+
 export const LC_OPENNING_DATES = [
   { value: "از تاریخ گشایش", label: "از تاریخ گشایش" },
   { value: "از تاریخ ابلاغ", label: "از تاریخ ابلاغ" },
   { value: "از تاریخ حمل/بارنامه", label: "از تاریخ حمل/بارنامه" },
   { value: "از تاریخ فاکتور", label: "از تاریخ فاکتور" },
-  { value: "از تاریخ ارائه اسناد به بانک/ معامله اسناد/ رسید دریافت بانک", label: "از تاریخ ارائه اسناد به بانک/ معامله اسناد/ رسید دریافت بانک" },
+  {
+    value: "از تاریخ ارائه اسناد به بانک/ معامله اسناد/ رسید دریافت بانک",
+    label: "از تاریخ ارائه اسناد به بانک/ معامله اسناد/ رسید دریافت بانک",
+  },
 ];
 
 export const LC_SETTLEMENT_DATES = [
@@ -34,3 +39,23 @@ export const SECOND_SLIDE_DOCS = [
   { label: "گواهی بازرسی", value: "govahibazrasi" },
   { label: "صورت مجلس تحویل و تحول", value: "tahvilotahavol" },
 ];
+
+export const TOAST_CONFIG = {
+  position: "top-center" as const,
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: false,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: "colored" as const,
+  transition: Bounce,
+};
+
+export const MODAL_CLASSES = {
+  overlay:
+    "mx-auto my-auto w-full h-full fixed inset-0 bg-black bg-opacity-40 flex justify-center items-center z-50 overflow-y-auto",
+  container: "bg-white p-6 rounded-lg shadow-lg relative",
+  closeButton:
+    "min-w-0 gap-0 w-7 h-7 border-none flex justify-center items-center text-center absolute bg-red-600 p-1 top-2 left-2 text-white text-xl rounded-full hover:bg-slate-100 hover:text-red-600 transition-all duration-300",
+};
