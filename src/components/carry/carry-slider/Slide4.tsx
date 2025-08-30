d3e4fc829e7885d82dc35411663fa85f311bf380
arashms1994 <arash.ms1994@gmail.com>
2025-08-30 10:22:26 +0330

    Feat: Add date picker to slides

--- src/components/carry/carry-slider/Slide4.tsx
+++ src/components/carry/carry-slider/Slide4.tsx
@@ -1,9 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useUploadedFiles } from "@/hooks/useUploadedFiles";
 import type { ICarrySlideProps } from "@/utils/type";
 import { useQueryClient } from "@tanstack/react-query";
 import { BASE_URL } from "@/api/base";
 import UploadSection from "@/components/ui/UploadSection";
+import PersianDatePicker from "@/components/persian-date-picker/PersianDatePicker";
+import DateObject from "react-date-object";
+import persian from "react-date-object/calendars/persian";
+import persian_fa from "react-date-object/locales/persian_fa";
+import gregorian from "react-date-object/calendars/gregorian";
+import gregorian_en from "react-date-object/locales/gregorian_en";
 
 const Slide4: React.FC<ICarrySlideProps> = ({
   faktorNumber,
@@ -11,6 +17,8 @@ const Slide4: React.FC<ICarrySlideProps> = ({
   setUploadedFiles,
   carryPhaseGUID,
 }) => {
+  const [selectedDate, setSelectedDate] = useState<string | null>(null);
+
   const label = "رسید بانک";
   const subFolder = carryPhaseGUID || "";
   const docType = "residebank";
@@ -34,16 +42,57 @@ const Slide4: React.FC<ICarrySlideProps> = ({
     });
   };
 
+  const handleSubmit = (event: React.FormEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    if (selectedDate) {
+      const dateObject = new DateObject({
+        date: selectedDate,
+        calendar: persian,
+        locale: persian_fa,
+      });
+
+      const gregorianDateObject = dateObject.convert(gregorian, gregorian_en);
+      const gregorianDate = gregorianDateObject.format("M/D/YYYY");
+
+      console.log("تاریخ شمسی:", selectedDate);
+      console.log("تاریخ میلادی:", gregorianDate);
+    } else {
+      console.log("تاریخی انتخاب نشده است");
+    }
+  };
+
   return (
-    <div className="flex flex-col justify-center items-center gap-5">
-      <UploadSection
-        orderNumber={faktorNumber}
-        subFolder={subFolder}
-        docType={docType}
-        label={label}
-        onUploadComplete={handleUploadComplete}
-      />
-    </div>
+    <>
+      <div className="flex flex-col justify-center items-center gap-5">
+        <UploadSection
+          orderNumber={faktorNumber}
+          subFolder={subFolder}
+          docType={docType}
+          label={label}
+          onUploadComplete={handleUploadComplete}
+        />
+      </div>
+
+      <div className="py-5">
+        <div className="w-full max-w-[500px] flex justify-between items-center gap-5">
+          <label className="text-[22px] font-medium">تاریخ رسید بانک:</label>
+          <PersianDatePicker
+            value={selectedDate}
+            onChange={(date: string) => setSelectedDate(date)}
+          />
+        </div>
+
+        <div className="flex justify-center items-center">
+          <button
+            type="submit"
+            className="border-none rounded-lg min-w-[200px] mt-5 p-3 text-[18px] font-semibold bg-blue-600 text-white transition-all duration-300 cursor-pointer hover:bg-blue-900"
+            onClick={handleSubmit}
+          >
+            ثبت تاریخ رسید بانک
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
